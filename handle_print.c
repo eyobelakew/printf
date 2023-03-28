83b3b5f769dd4a6497b7aad5bf7076253bb38571
Eyob L. Getaneh <122857983+eyobelakew@users.noreply.github.com>
2023-03-28 13:13:48 +0300

    testing

--- handle_print.c
+++ handle_print.c
@@ -17,11 +17,20 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
-		{'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {'d', print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hexadecimal},
+		{'X', print_hexa_upper},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13string},
+		{'r', print_reverse},
+		{'S', print_non_printable},
+		{'p', print_pointer},
+		{'%', print_percent}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
